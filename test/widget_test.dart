5341cb6ec30a5de36ae1e964bbbb4f25d7008ec2
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-08-10 20:45:36 -0300

    removed tests

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:basso_hoogerheide/app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Test description', (tester) async {
-    await tester.pumpWidget(const App());
-  });
-}
