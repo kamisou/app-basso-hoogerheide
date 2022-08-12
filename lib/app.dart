0268dd2903a3755f2e38920bcdf42e23a4b1bd4d
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-08-12 13:28:47 -0300

    removed controller for now to focus on screens

--- lib/app.dart
+++ lib/app.dart
@@ -1,39 +1,22 @@
-import 'package:basso_hoogerheide/constants/app_configuration.dart';
 import 'package:basso_hoogerheide/constants/theme_data.dart';
-import 'package:basso_hoogerheide/controllers/profile.dart';
-import 'package:basso_hoogerheide/interfaces/encrypted_storage.dart';
 import 'package:basso_hoogerheide/pages/home.dart';
 import 'package:basso_hoogerheide/pages/login.dart';
 import 'package:basso_hoogerheide/pages/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class App extends ConsumerWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   final AppTheme _appTheme = const AppTheme();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       color: _appTheme.dark.colorScheme.primary,
       darkTheme: _appTheme.dark,
       debugShowCheckedModeBanner: false,
       home: SplashPage(
-        initialWork: () async {
-          final encryptedStorage = ref.read(storageProvider);
-          final config = ref.read(appConfigProvider);
-          await encryptedStorage.initialize();
-
-          final token = encryptedStorage.read(config.sessionTokenStorageKey);
-          if (token != null) {
-            final profile = ref.read(profileProvider);
-            profile.getUser();
-            return '/home';
-          }
-
-          return '/login';
-        },
+        initialWork: () async => '/home',
       ),
       restorationScopeId: 'basso_hoogerheide',
       routes: {
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,10 @@
-import 'package:basso_hoogerheide/controllers/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final profile = ref.watch(profileProvider);
-    return Scaffold(
-      body: profile.user.when(
-        data: (user) => Text(user!.name),
-        error: (_, __) => const SizedBox.shrink(),
-        loading: () => const Center(child: CircularProgressIndicator()),
-      ),
-    );
+  Widget build(BuildContext context) {
+    return const Scaffold();
   }
 }
