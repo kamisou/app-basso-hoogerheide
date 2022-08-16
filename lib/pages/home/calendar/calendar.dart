0b6c6f717ec87e449983b923ee5601a9bab65294
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-08-16 09:42:07 -0300

    replaced calendar listview with an infinite listview

--- lib/pages/home/calendar/calendar.dart
+++ lib/pages/home/calendar/calendar.dart
@@ -2,6 +2,7 @@ import 'package:basso_hoogerheide/data_objects/calendar/calendar_event.dart';
 import 'package:basso_hoogerheide/pages/home/calendar/day.dart';
 import 'package:basso_hoogerheide/widgets/base_page_body.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_listview/infinite_listview.dart';
 
 class CalendarPage extends HomePageBody {
   const CalendarPage({super.key});
@@ -9,12 +10,12 @@ class CalendarPage extends HomePageBody {
   @override
   Widget build(BuildContext context) {
     final DateTime today = DateTime.now();
-    return ListView.builder(
+    return InfiniteListView.builder(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       itemBuilder: (_, index) => Padding(
         padding: const EdgeInsets.only(bottom: 32),
         child: DayWidget(
-          date: DateTime(2022, 6, 25).add(Duration(days: index)),
+          date: DateTime.now().add(Duration(days: index)),
           today: today,
           events: [
             const CalendarEvent(
@@ -37,6 +38,34 @@ class CalendarPage extends HomePageBody {
         ),
       ),
     );
+    // return ListView.builder(
+    //   padding: const EdgeInsets.symmetric(horizontal: 20),
+    //   itemBuilder: (_, index) => Padding(
+    //     padding: const EdgeInsets.only(bottom: 32),
+    //     child: DayWidget(
+    //       date: DateTime(2022, 6, 25).add(Duration(days: index)),
+    //       today: today,
+    //       events: [
+    //         const CalendarEvent(
+    //           startTime: TimeOfDay(hour: 8, minute: 0),
+    //           endTime: TimeOfDay(hour: 8, minute: 50),
+    //           title: 'Pagamento para João da Silva',
+    //           description:
+    //               'Delenit est justo odio vero consetetur adipiscing amet sit dolore.',
+    //           color: Color(0xFFA81818),
+    //         ),
+    //         if (index % 16 == 0)
+    //           const CalendarEvent(
+    //             startTime: TimeOfDay(hour: 9, minute: 0),
+    //             endTime: TimeOfDay(hour: 15, minute: 00),
+    //             title: 'Atendimento Maria Silva',
+    //             description: 'Delenit est justo odio vero consetetur.',
+    //             color: Color(0xFF1840A8),
+    //           ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 
   @override
