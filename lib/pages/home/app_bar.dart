b98230fb367dbeedd2cbb72b9acac17195a2850d
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-08-15 21:44:58 -0300

    fixed appbar tabs (for now)

--- lib/pages/home/app_bar.dart
+++ lib/pages/home/app_bar.dart
@@ -1,16 +1,49 @@
 import 'package:basso_hoogerheide/widgets/avatar_circle.dart';
 import 'package:flutter/material.dart';
 
-class HomeAppBar extends StatelessWidget {
+class HomeAppBar extends StatefulWidget {
   const HomeAppBar({
     super.key,
     required this.controller,
     required this.pageTitles,
   });
 
-  final ScrollController controller;
+  final PageController controller;
 
   final List<String> pageTitles;
+
+  @override
+  State<HomeAppBar> createState() => _HomeAppBarState();
+}
+
+class _HomeAppBarState extends State<HomeAppBar> {
+  static const double _tabWidth = 100;
+
+  final ScrollController _controller = ScrollController();
+
+  int _pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_controllerListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.controller.removeListener(_controllerListener);
+    _controller.dispose();
+  }
+
+  void _controllerListener() {
+    final double page = widget.controller.page!;
+    final int pageIndex = page.round();
+    _controller.position.jumpTo(page * 100);
+    if (_pageIndex != pageIndex) {
+      setState(() => _pageIndex = pageIndex);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,24 +66,30 @@ class HomeAppBar extends StatelessWidget {
                 ],
                 end: Alignment(.65, 0),
               ).createShader(bounds),
-              child: ListView.builder(
-                itemCount: pageTitles.length,
-                itemExtent: 100,
-                physics: const NeverScrollableScrollPhysics(
-                  parent: ClampingScrollPhysics(),
-                ),
+              child: ListView(
+                controller: _controller,
+                physics: const NeverScrollableScrollPhysics(),
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (_, index) => Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      pageTitles[index],
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  ...List.generate(
+                    widget.pageTitles.length,
+                    (index) => Container(
+                      alignment: Alignment.centerLeft,
+                      width: _tabWidth,
+                      child: Text(
+                        widget.pageTitles[index],
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  fontWeight: index == _pageIndex
+                                      ? FontWeight.bold
+                                      : FontWeight.normal,
+                                ),
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width),
+                ],
               ),
             ),
           ),
