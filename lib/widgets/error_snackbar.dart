7781c0f33a578964672be4b3b2d86e25306ab95d
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-09-09 17:56:28 -0300

    error_snackbar throwing discarded exception to be handled

--- lib/widgets/error_snackbar.dart
+++ lib/widgets/error_snackbar.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ErrorSnackbar<E extends Object?> {
+class ErrorSnackbar<E extends Object> {
   const ErrorSnackbar({
     required this.content,
   });
 
   final Widget Function(BuildContext, E) content;
 
-  void show(BuildContext context, Object? error) {
-    if (error is! E) return;
+  void show(BuildContext context, Object error) {
+    if (error is! E) throw error;
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: content(context, error)),
     );
