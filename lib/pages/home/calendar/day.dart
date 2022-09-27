850ec342b78cbd7b87b18bf77147c75e5ef0f4e9
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-09-27 11:05:02 -0300

    added event id to event cards to attempt state restoration

--- lib/pages/home/calendar/day.dart
+++ lib/pages/home/calendar/day.dart
@@ -67,7 +67,10 @@ class DayWidget extends StatelessWidget {
         const SizedBox(height: 24),
         if (events != null)
           ListView.separated(
-            itemBuilder: (_, index) => EventCard(event: events![index]),
+            itemBuilder: (_, index) => EventCard(
+              key: ValueKey(events![index].id),
+              event: events![index],
+            ),
             separatorBuilder: (_, __) => const SizedBox(height: 10),
             itemCount: events!.length,
             padding: EdgeInsets.zero,
