3fee1249c0571de8dafbd5cac19b43bc6395eb26
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2022-10-07 17:39:17 -0300

    contact tile isNotEmpty

--- lib/pages/home/contacts/contact_tile.dart
+++ lib/pages/home/contacts/contact_tile.dart
@@ -48,7 +48,7 @@ class _ContactTileState extends ConsumerState<ContactTile> {
                   ),
                   Row(
                     children: [
-                      if (widget.contact.telephone?.isEmpty ?? true) ...[
+                      if (widget.contact.telephone?.isNotEmpty ?? false) ...[
                         Flexible(
                           child: KeyValueText(
                             keyString: 'Tel',
@@ -58,7 +58,7 @@ class _ContactTileState extends ConsumerState<ContactTile> {
                         ),
                         const SizedBox(width: 8),
                       ],
-                      if (widget.contact.cellphone?.isEmpty ?? true)
+                      if (widget.contact.cellphone?.isNotEmpty ?? false)
                         Flexible(
                           child: KeyValueText(
                             keyString: 'Cel',
